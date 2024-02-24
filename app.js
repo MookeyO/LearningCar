03711cd2320d7bbd707414c69ebca8faf84b50b7
Isaiah White <isaiahwhite@teamufw.com>
2024-02-23 21:38:47 -0700

    adding node server and camera.py

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
+app.use(express.static('public'));
+app.use
