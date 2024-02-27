8774f1bf3fa7ac6df1d6d0de3d467cf65554ed3b
Isaiah White <isaiahwhite@teamufw.com>
2024-02-26 20:49:14 -0700

    attempting fix for autnomous (buzzer not working)

--- autonomous.py
+++ autonomous.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
+#autonomous.py
 import RPi.GPIO as GPIO
 import time
 import activeBuzzer
 
 
-
-
-
-
-
+#set initial state
+GPIO.setmode(GPIO.BCM)
 state = 'Start'
 
 while True:
 
     if state == 'Start':
-        activeBuzzer.activate_buzzer
+        activeBuzzer.activate_buzzer()
         state = 'Stop'
         continue
     elif state == 'Stop':
